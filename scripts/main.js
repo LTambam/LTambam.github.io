9061bcc5a75b4d5f242866138625f6b2313ec446
LTambam <35719319+LTambam@users.noreply.github.com>
2022-09-08 16:00:56 -0400

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const myHeading = document.querySelector('h1');
+
+
+// let myText = "Luke Tambakis";
+
+// myHeading.textContent = myText;
+
+document.querySelector('html').addEventListener('click', function () {
+    alert('Ouch! Stop poking me!');
+  });
